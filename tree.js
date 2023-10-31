7b677282a8f6c72db040be4a6ea8a72c1e16d3d2
spathiyal <spathiyal@gmail.com>
2023-10-31 11:32:59 -0700

    updated tree.js

--- tree.js
+++ tree.js
@@ -15,20 +15,58 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    let sum = this.root.val;
+    function addNodeValue(node) {
+      for (let child of node.children) {
+        sum += child.val;
+        // recursive call if the child has children
+        if (child.children.length > 0) {
+          addNodeValue(child);
+        }
+      }
+    }
+    addNodeValue(this.root);
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
-
+    //find the node value is even
+    let countEven = this.root.val % 2 === 0 ? 1 : 0;
+    function countEvenNodeValue(node) {
+      for (let child of node.children) {
+        if (child.val % 2 === 0) {
+          countEven++;
+        }
+        //if child has children recursive call of the function again.
+        if (child.children.length > 0) {
+          countEvenNodeValue(child);
+        }
+      }
+    }
+    countEvenNodeValue(this.root);
+    return countEven;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
-
+    let count = this.root.val > lowerBound ? 1 : 0;
+    function countGreatNodeVal(node) {
+      for (let child of node.children) {
+        if (child.val > lowerBound) {
+          count++;
+        }
+        //if child has children recursive call of the function again.
+        if (child.children.length > 0) {
+          countGreatNodeVal(child);
+        }
+      }
+    }
+    countGreatNodeVal(this.root);
+    return count;
   }
 }
 
